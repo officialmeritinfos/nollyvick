0bb372794601b755f253865eaf7e4dde7f06cd88
Anon Dev <michaelerastus9@gmail.com>
2024-09-11 09:22:37 +0100

    Updated address

--- resources/views/user/investment_detail.blade.php
+++ resources/views/user/investment_detail.blade.php
@@ -117,7 +117,7 @@
                 <div class="email-content">
                     <p class="report">
                         You are to send <b>{{number_format($investment->amount,2)}} of {{$investment->asset}}</b>
-                        to the address <b style="font-size:20px;" id="address">{{$investment->wallet}}</b>.<br>
+                        to the address <b style="font-size:20px;word-break: break-word;" id="address">{{$investment->wallet}}</b>.<br>
                         After making payment, contact support for instant crediting.<br/>
 
                         Alternatively, you can scan the Barcode above to get the wallet address:<br/>
